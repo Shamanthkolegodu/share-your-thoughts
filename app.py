e5b71b4bac36798c35c9695784f003d25bb92e77
Shamanth K M <shamanthkolegodu@gmail.ci=om>
2020-08-15 22:31:06 +0530

    Update app.py
    use of render template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def homepage():
-    return "<h1>Homepage</h1>"
+    return render_template('home.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
