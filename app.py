4e3686b1e96c59ce23bd7dffc2bc7e1e2583f2ba
Shamanth K M <shamanthkolegodu@gmail.ci=om>
2020-08-16 19:01:21 +0530

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,3 +9,16 @@ def homepage():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
