5da59f5c7294ca3e29ea112965e12b33b38b04ce
Shamanth K M <shamanthkolegodu@gmail.ci=om>
2020-08-26 00:17:03 +0530

    update

---
+++ static/home.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  // Activate the Carousel. Pause it when you move the mouse over it
+  $("#myCarousel").carousel({ interval: 500, pause: "hover" });
+
+  // Enable Carousel Indicators
+  $(".item1").click(function () {
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function () {
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function () {
+    $("#myCarousel").carousel(2);
+  });
+
+  // Enable Carousel Controls
+  $(".carousel-control-prev").click(function () {
+    $("#myCarousel").carousel("prev");
+  });
+  $(".carousel-control-next").click(function () {
+    $("#myCarousel").carousel("next");
+  });
+});
